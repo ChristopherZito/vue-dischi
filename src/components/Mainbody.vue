15f5703e5e60dbf631536711d85853ff1af6608b
christopherzito <christopherzito11@gmail.com>
2021-11-29 13:38:08 +0100

    update ricerca per generi

--- src/components/Mainbody.vue
+++ src/components/Mainbody.vue
@@ -1,53 +1,74 @@
 <template>
-  <div id="container">
+    <div id="container">
+        <div>
+            <Search @genere="searchType"/>
+        </div>
         <div id="load" v-if="loading === false">
             Loading...
         </div>
         <Album 
         v-else
-        v-for="album, i in albums" :key="i"
+        v-for="album, i in newAlbum" :key="i"
         :element="album"/>
-  </div>
+    </div>
 </template>
 
 <script>
 import axios from "axios"
 import Album from '@/components/Album.vue'
+import Search from '@/components/search.vue'
 
 export default {
-  name: 'Mainbody',
-  components: {
-    Album
-  },
-  data(){
-      return {
-          loading: false,
-          albums: [],
-          urlDisk:"https://flynn.boolean.careers/exercises/api/array/music",
-      }
-  },
-  created() {
-      this.subElement();
-  },
-  methods: {
-      subElement() {
-          axios
-          .get(this.urlDisk)
-          .then((block) => {
-              /* console.log(block.data.response); */
-              /* console.log(this.loading , "&" ,this.albums); */
-              this.albums = block.data.response;
-              this.loading = true;
-              /* console.log(this.loading , "&" ,this.albums); */
-          })
-      }
-  }
+    name: 'Mainbody',
+    components: {
+        Album,
+        Search
+},
+data(){
+    return {
+        searchText:"",
+        loading: false,
+        albums: [],
+        urlDisk:"https://flynn.boolean.careers/exercises/api/array/music",
+    }
+},
+created() {
+    this.subElement();
+},
+computed:{
+    newAlbum() {
+        if(this.searchText == ""){
+            return this.albums;
+        }
+        return this.albums.filter((tipo) => {
+            return tipo.genre.toLowerCase().includes(this.searchText.toLowerCase())
+        })   
+    }
+},
+methods: {
+    searchType(style){
+        this.searchText = style;
+
+    },
+    subElement() {
+        axios
+        .get(this.urlDisk)
+        .then((block) => {
+            /* console.log(block.data.response); */
+            /* console.log(this.loading , "&" ,this.albums); */
+            this.albums = block.data.response;
+            this.loading = true;
+            /* console.log(this.loading , "&" ,this.albums); */
+        })
+    },
+}
 }
 </script>
 
 <style scoped lang="scss">
 #container {
     width: 60%;
+    height: 1000px;
     margin: 0 auto;
     padding: 50px 0;
 
---
+++ src/components/search.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <select v-model="type" @change="$emit(`genere`, type)">
+        <option disabled value="">select one genre</option>
+        <option>Rock</option>
+        <option>Pop</option>
+        <option>Jazz</option>
+        <option>Metal</option>
+    </select>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+      return {
+          type: "",
+      }
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
